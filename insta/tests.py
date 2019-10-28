ede8475a6e3f840291d981b446568868324cd672
i-Krypt <vamay505@gmail.com>
2019-10-28 08:54:11 +0300

    tested the models

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from .models import Post,Comment,Profile
 # Create your tests here.
+class Post(TestCase):
+    #set up method
+    def setUp(self):
+        self.caption= Post(location='caption')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.caption,Post))
+    def tearDown(self):
+        Post.objects.all().delete()
+    def test_save_method(self):
+        self.caption.save_location()
+        post = Post.objects.all()
+        self.assertTrue(len(post)>0)
+    def test_delete_method(self):
+        self.caption.delete_location('caption')
+        post = Post.objects.all()
+        self.assertTrue(len(post)==0)
+class ProfileTestClass(TestCase):
+   def setUp(self):
+       self.profile=Profile()
+       self.profile.save_profile()
+   def test_instance(self):
+       self.assertTrue(isinstance(self.profile,Profile))
+   def test_save_profile(self):
+       self.profile.save_profile()
+       profiles = Profile.objects.all()
+       self.assertTrue(len(profiles) > 0)
+   def test_delete_profile(self):
+       self.profile.delete_profile()
+       profile = Profile.objects.all()
+       self.assertTrue(len(profile) == 0)
